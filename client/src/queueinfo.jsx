042c164713b1a969787b5e1f6ec2b0573218a482
Eugene Soo <eugene@soo.sg>
2017-10-09 05:50:21 +0000

    remove queueinfo as not in use

--- client/src/queueinfo.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import QueueInfo from './components/customer/QueueInfo.jsx';
-
-ReactDOM.render((<QueueInfo />), document.getElementById('queueinfo'));
