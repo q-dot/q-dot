bde3f5704c1320593b032e806aed5fdf92e2ba94
Christie Villanueva <christiejvillanueva@gmail.com>
2017-10-10 16:48:25 -0700

    Fix incorrect syntax for inserting restaurant row, add address to test data

--- database/testData.js
+++ database/testData.js
@@ -1,4 +1,4 @@
-const restaurant = {name: 'Chicken on Fire', phone: '(415) 555-4555', image: '../images/houseofprimerib.jpg', status: 'Open', 'average_wait': 10, 'total_wait': 10};
+const restaurant = {name: 'Chicken on Fire', phone: '(415) 555-4555', image: '../images/houseofprimerib.jpg', address: '1234 Market St', status: 'Open', 'average_wait': 10, 'total_wait': 10};
 
 module.exports = {
   restaurant: restaurant
