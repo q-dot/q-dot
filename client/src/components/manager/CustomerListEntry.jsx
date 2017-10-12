41d2e629bc746aac99f79ea17d40ff1c16a3c4f1
DanDai <ddai85@gmail.com>
2017-10-11 19:08:14 -0700

    Added timer to manager view which is displayed when customer is notified

--- client/src/components/manager/CustomerListEntry.jsx
+++ client/src/components/manager/CustomerListEntry.jsx
@@ -1,25 +1,67 @@
 import React from 'react';
 
-const CustomerListEntry = (props) => {
-  return (
-    <div className="row panel-body">
-      <div className="col-md-7">
-        <h3 className="customer-entry-title">{props.queue.customer.name}</h3>
-        <div className="row">
-          <p className="col-md-6"><i className="fa fa-mobile fa-fw" aria-hidden="true"></i> {props.queue.customer.mobile}</p>
-          {props.queue.customer.email ? <p><i className="fa fa-envelope-o fa-fw" aria-hidden="true"></i> {props.queue.customer.email}</p> : null}
+class CustomerListEntry extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      timer: ''
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.startTimer();
+    this.props.notiCustomer(this.props.queue.id, this.props.queue.customer);
+  }
+
+  startTimer() {
+    let maxTime = 180000;
+
+    for (let i = 0; i <= 180000; i += 1000) {
+      setTimeout(() => {
+        let time = (maxTime - i) / 1000;
+        let min = Math.floor(time / 60);
+        let sec = Math.floor(time % 60);
+        sec = ('0' + sec).slice(-2);
+        let timeStr = min.toString() + ':' + sec.toString();
+
+        this.setState({timer: timeStr});
+        
+      }, i);
+    }
+  }
+
+  render() {
+    return (
+      <div className="row panel-body">
+        <div className="col-md-7">
+          <div className="customer-entry-head">
+            <h3 className="customer-entry-text">{this.props.queue.customer.name}</h3>
+            <p className="customer-entry-text timer">{this.state.timer}</p>
+          </div>
+          <div className="row">
+            <p className="col-md-6"><i className="fa fa-mobile fa-fw" aria-hidden="true"></i> {this.props.queue.customer.mobile}</p>
+            {this.props.queue.customer.email ? <p><i className="fa fa-envelope-o fa-fw" aria-hidden="true"></i> {this.props.queue.customer.email}</p> : null}
+          </div>
+          <div className="row">
+            <p className="col-md-6"><i className="fa fa-users fa-fw" aria-hidden="true"></i> {this.props.queue.size}</p>
+            <p><i className="fa fa-clock-o fa-fw" aria-hidden="true"></i> {this.props.queue.wait} mins</p>
+          </div>
         </div>
-        <div className="row">
-          <p className="col-md-6"><i className="fa fa-users fa-fw" aria-hidden="true"></i> {props.queue.size}</p>
-          <p><i className="fa fa-clock-o fa-fw" aria-hidden="true"></i> {props.queue.wait} mins</p>
+        <div className="col-md-5 row">
+          <button className="btn-primary btn-sm entry-button" data-dismiss="modal" onClick={() => this.props.showModal(this.props.queue)}><i className="fa fa-user-times fa-fw" aria-hidden="true"></i>Remove</button>
+          <button className="btn-success btn-sm entry-button" onClick={this.handleClick}><i className="fa fa-bullhorn fa-fw" aria-hidden="true"></i>Ready </button>
         </div>
       </div>
-      <div className="col-md-5 row">
-        <button className="btn-primary btn-sm entry-button" data-dismiss="modal" onClick={() => props.showModal(props.queue)}><i className="fa fa-user-times fa-fw" aria-hidden="true"></i>Remove</button>
-        <button className="btn-success btn-sm entry-button" onClick={() => props.notiCustomer(props.queue.id, props.queue.customer)}><i className="fa fa-bullhorn fa-fw" aria-hidden="true"></i>Ready </button>
-      </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default CustomerListEntry;
+
+
+
+
+
+
