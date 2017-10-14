64d5e1466b46feb7dee084063e30a9777c693435
laraismael1 <laraismael123@gmail.com>
2017-10-13 19:17:27 -0700

    add home button to all pages

--- client/src/components/customer/CustomerNav.jsx
+++ client/src/components/customer/CustomerNav.jsx
@@ -3,20 +3,13 @@ import React from 'react';
 // nav bar
 const CustomerNav = () => (
   <div className="customer-nav-bar">
-    <ul id="dropdown1" className="dropdown-content">
-      <li><a href="/">home</a></li>
-      <li className="divider"></li>
-      <li><a href="../manager">manager</a></li>
-      <li><a href="../createRestaurant">add restaurant</a></li>
+    <ul className="nav-content">
+      <li className="nav-li-right"><a className="waves-effect waves-light btn" href="/">Home</a></li>
+      <li className="nav-li"><a className="waves-effect waves-light btn" href="../manager">Restaurant Manager</a></li>
+      <li className="nav-li"><a className="waves-effect waves-light btn" href="../createrestaurant">Add a Restaurant</a></li>
     </ul>
-    <nav>
-      <div className="nav-wrapper">
-        <ul className="nav-mobile hide-on-med-and-down">
-          <li><a className="dropdown-button" href="#!" data-activates="dropdown1">q.<i className="material-icons right">arrow_drop_down</i></a></li>
-        </ul>
-      </div>
-    </nav>
   </div>
 );
 
 export default CustomerNav;
+
